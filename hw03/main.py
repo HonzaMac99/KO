f9f32993649f87312c500bf0642512aa77690b16
macaljan_LinPC <macaljan@fel.cvut.cz>
2024-05-23 07:43:04 +0200

    update on hw03

--- hw03/main.py
+++ hw03/main.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-LAZY_CS = True
+np.set_printoptions(linewidth=500)
+
 
 opts = {
     "WLSACCESSID": "62cd638b-d118-4fc1-b04d-624506c02ad3",
@@ -12,53 +13,201 @@ opts = {
     "LICENSEID":   2481370,
 }
 
+# enum node types
+S_TYPE = 0
+C_TYPE = 1
+P_TYPE = 2
+T_TYPE = 3
+INF = 10000
 
-# use the academic licence
-with g.Env(params=opts) as env, g.Model(env=env) as m:
-    # model = g.Model()
 
+# function for dfs search
+def construct_path(G, visited, start_id, path, cap):
+    visited.append(start_id)
+
+    t_new_id = n_nodes-1
+    if start_id != t_new_id:
+
+        # check the forward connections
+        nodes_f = np.where(G[start_id, :, 2] >= 0)[0]
+        visited_f = np.isin(nodes_f, visited)
+        nodes_f = nodes_f[~visited_f]
+
+        # there must be empty room for some more flow
+        empty = G[start_id, nodes_f, 1] < G[start_id, nodes_f, 2]
+        nodes_id_forward = nodes_f[empty]
+
+        # check the backward connections
+        nodes_b = np.where(G[:, start_id, 2] >= 0)[0]
+        visited_b = np.isin(nodes_b, visited)
+        nodes_b = nodes_b[~visited_b]
+
+        # there must be empty room for lowering the flow
+        empty = G[nodes_b, start_id, 1] > G[nodes_b, start_id, 0]
+        nodes_id_backward = nodes_b[empty]
+
+        for node in nodes_id_forward:
+            u_i = G[start_id, node, 2]
+            f_i = G[start_id, node, 1]
+
+            path_new = path + [node]
+            cap_new = cap + [u_i - f_i]
+
+            construct_path(G, visited, node, path_new, cap_new)
+            if cap:
+                break
+
+        if cap is None:
+            for node in nodes_id_backward:
+                l_i = G[node, start_id, 0]
+                f_i = G[node, start_id, 1]
+
+                path_new = path + [node]
+                cap_new = cap + [f_i - l_i]
+
+                construct_path(G, visited, node, path_new, cap_new)
+                if cap:
+                    break
+
+
+if __name__ == "__main__":
     if len(sys.argv) < 3:
         # Default file names
-        input_file = "in.txt"
+        input_file = "in2.txt"
         output_file = "out.txt"
     else:
         input_file = sys.argv[1]
         output_file = sys.argv[2]
 
-    G = {}
     with open(input_file, 'r') as f:
         input_data = f.read().split("\n")
         [n_cust, n_prod] = list(map(int, input_data[0].split(" ")))
-        G["s"] = []
+        n_nodes = n_cust + n_prod + 2
+
+        # s_id = 0
+        s_id = n_nodes-2
+        t_id = n_nodes-1
+
+        # will be added later to the graph
+        s_new_id = n_nodes
+        t_new_id = n_nodes + 1
+        n_nodes += 2
+
+        G = np.ones((n_nodes, n_nodes, 3)) * -1   # -INF
 
         for i in range(n_cust):
-            ci = f"c{i+1}"
+            c_id = i
             cust_info = list(map(int, input_data[i+1].split(" ")))
             l_i, u_i = cust_info[:2]
-            G["s"].append([ci, l_i, -1, u_i])
+            G[s_id, c_id, :] = [l_i, -1, u_i]
             cust_prods = cust_info[2:]
             for p in cust_prods:
-                pi = f"p{p}"
-                e = [pi, 0, -1, 1]
-                G[ci] = G[ci] + [e] if ci in G else [e]
+                p_id = n_cust + p-1
+                G[c_id, p_id] = [0, -1, 1]
         demands = list(map(int, input_data[-1].split(" ")))
-        for i, d in enumerate(demands):
-            pi = f"p{i+1}"
-            G[pi] = [["t", d, -1, np.inf]]
+        for i, v in enumerate(demands):
+            p_id = n_cust + i
+            G[p_id, t_id, :] = [v, -1, INF]
 
-    # init step 1
-    G["t"] = [["s", 0, -1, np.inf]]
+    G_orig = G.copy()
 
-    # init step 2
+    # step 1 - compute balances
+    G[t_id, s_id, :] = [0, -1, INF]
 
+    print(G[:-2, :-2, 0])
 
+    enabled = G[:, :, 0] >= 0
+    l_in_arr = np.sum(G[:, :, 0]*enabled, axis=0)
+    l_out_arr = np.sum(G[:, :, 0]*enabled, axis=1)
+    balance_arr = l_in_arr - l_out_arr
 
+    G[:, :, 2][enabled] -= G[:, :, 0][enabled]
+    G[:, :, 0][enabled] = 0
 
-    ordering = []
+    # step 2 - transform to zero lbs, apply balances
+
+    # s_new_id = n_nodes
+    # t_new_id = n_nodes + 1
+    # n_nodes += 2
+
+    for i, balance in enumerate(balance_arr):
+        if balance > 0:
+            G[s_new_id, i, :] = [0, -1, balance]
+        if balance < 0:
+            G[i, t_new_id, :] = [0, -1, -balance]
+
+    # step 3 - initialize the flow
+    enabled = G[:, :, 0] == 0
+    G[:, :, 1][enabled] = 0
+
+    # step 4 - Ford Fulkerson
+    while True:
+        path = []
+        visited = []
+        capacities = []
+
+        start_id = s_new_id
+        path.append(start_id)
+        construct_path(G, visited, start_id, path, capacities)
+
+        if path and capacities and min(capacities) > 0:
+            cap_min = min(capacities)
+            for i in range(len(path)-1):
+                u_id = path[i]
+                v_id = path[i+1]
+
+                if G[u_id, v_id, 0] < 0:
+                    G[v_id, u_id, 1] -= cap_min
+                else:
+                    G[u_id, v_id, 1] += cap_min
+        else:
+            break
+
+    partial_result = G[:, :, 1] + G_orig[:, :, 0]
+
+    # step 5 - initialize flow of the original graph
+    G = G_orig[:-2, :-2, :]
+    G[:, :, 1] = partial_result[:-2, :-2]
+
+    # step 6 - Ford Fulkerson again
+    while True:
+        path = []
+        visited = []
+        capacities = []
+
+        start_id = s_id
+        path.append(start_id)
+        construct_path(G, visited, start_id, path, capacities)
+
+        if path and capacities and min(capacities) > 0:
+            cap_min = min(capacities)
+            for i in range(len(path)-1):
+                u_id = path[i]
+                v_id = path[i+1]
+
+                if G[u_id, v_id, 0] < 0:
+                    G[v_id, u_id, 1] -= cap_min
+                else:
+                    G[u_id, v_id, 1] += cap_min
+        else:
+            break
+
+    v_min = G[n_cust:n_cust + n_prod, -1, 1]
+    products = G[0:n_cust, n_cust:n_cust + n_prod, 1]
+    products[products < 0] = 0
+    v_computed = np.sum(G[0:n_cust, n_cust:n_cust + n_prod, 1], axis=0)
+    if np.all(v_computed >= v_min):
+        result = G[0:n_cust, n_cust:n_cust + n_prod, 1]
+    else:
+        result = None
+
     with open(output_file, 'w') as f:
-        for i in range(len(ordering)-1):
-            f.write(f"{ordering[i]} ")
-
-    if __name__ == "__main__":
-        ...
+        if result is None:
+            f.write(f"-1\n")
+        else:
+            for r in result:
+                # Get the indices where the value is equal to 1
+                indices = np.where(r == 1)[0] + 1
+                if indices.size != 0:
+                    f.write(' '.join(map(str, indices)) + '\n')
 
---
+++ hw03/test/main.py
@@ -0,0 +1,254 @@
+#!/usr/bin/env python3
+import sys
+import numpy as np
+
+np.set_printoptions(linewidth=500)
+
+ld_ori_id = 3
+u_id = 2
+f_id = 1
+l_id = 0
+infinity = 1000000
+counter = 0
+
+
+class Graph:
+    def __init__(self,num_nodes,C,P):
+        # classical edge matrix, 4 stands for l,f,l,b
+        self.C = C
+        self.P = P
+        self.counter = 0
+        self.counter2 = 0
+        self.body = np.ones((num_nodes,num_nodes,4)) * -1
+        self.num_nodes = num_nodes
+
+    def dfs(self, start, visited, path = None, capacity = None):
+        visited.append(start)
+        if self.counter >= 277:
+            print(self.counter2)
+        if capacity is None:
+            capacity = []
+        if start != self.num_nodes-1:
+            nodes_id = [x for x in range(self.num_nodes)]
+
+            nodes_id_forward = np.where(self.body[start,:,u_id] > -1)[0]
+
+            mask = np.isin(nodes_id_forward, visited, invert=True)
+
+            nodes_id_forward = nodes_id_forward[mask]
+
+            nodes_id_backward = np.where(self.body[:,start, u_id] > -1)[0]
+
+            mask = np.isin(nodes_id_backward, visited, invert=True)
+            nodes_id_backward = nodes_id_backward[mask]
+
+            condition = self.body[start,nodes_id_forward,f_id] < self.body[start,nodes_id_forward,u_id]
+            nodes_id_forward = nodes_id_forward[condition]
+
+            condition = self.body[nodes_id_backward,start,f_id] > self.body[nodes_id_backward,start,l_id]
+            nodes_id_backward = nodes_id_backward[condition]
+
+            for next in nodes_id_forward:
+                u_i = self.body[start,next,u_id]
+                f_i = self.body[start,next,f_id]
+                self.dfs(next, visited,path + [next], capacity + [u_i - f_i])
+                if self.capacity:
+                    break
+            if self.capacity is None:
+                for next in nodes_id_backward:
+                    l_i = self.body[next, start, l_id]
+                    f_i = self.body[next, start, f_id]
+                    self.dfs(next,visited, path + [next], capacity + [f_i - l_i])
+                    if self.capacity:
+                        break
+        else:
+            self.capacity = capacity
+            self.path = path
+        self.counter2 += 1
+
+    def init_flow(self, flow = None):
+        if flow is not None:
+            self.body[:, :, f_id] = flow
+        else:
+            condition = self.body[:, :, l_id] == 0
+            self.body[:, :, f_id][condition] = 0
+
+    def ford_fulkerson(self):
+        a = self.body[:,:,l_id]
+        b =  self.body[:,:,u_id]
+        c = self.body[:,:,f_id]
+        d =  self.body[:,:,ld_ori_id]
+
+        while True:
+            print(self.counter)
+            self.counter += 1
+            if self.counter == 277:
+                print(4)
+            self.capacity = None
+            self.path = None
+            self.dfs(self.num_nodes-2,[],[self.num_nodes-2])
+            if self.path and self.capacity and min(self.capacity) > 0:
+                self.capacity = min(self.capacity)
+                c = self.body[:, :, f_id]
+                for i,from_id in enumerate(self.path[:-1]):
+                    to_id = self.path[i+1]
+
+                    #is backward
+                    if self.body[from_id,to_id,l_id] < 0:
+                        self.body[to_id,from_id,f_id] -= self.capacity
+
+                    #is forward
+                    else:
+                        self.body[from_id, to_id, f_id] += self.capacity
+            else:
+                break
+
+    def find_P(self):
+        visited = np.full((self.num_nodes),False)
+        s_id = -2
+        t_id = -1
+        visited[s_id] = True
+        path = [-2]
+        actual_node = s_id
+        min_capacity = infinity
+        while not visited[t_id]:
+            find_next = False
+            #forward
+            for i,u in enumerate(self.body[actual_node,:,u_id]):
+                if u < 0 or visited[i] == True:
+                    continue
+                f_i = self.body[actual_node,i,f_id]
+                if f_i < u:
+                    if u - f_i < min_capacity:
+                        min_capacity = u - f_i
+                    actual_node = i
+                    find_next = True
+                    break
+            if not find_next:
+                #backward
+                for i, l in enumerate(self.body[:,actual_node, l_id]):
+                    if l < 0 or visited[i] == True:
+                        continue
+                    f_i = self.body[i,actual_node, f_id]
+                    if f_i > l:
+                        if f_i  - l < min_capacity:
+                            min_capacity = f_i - l
+                        actual_node = i
+                        find_next = True
+                        break
+                if not find_next and not visited[t_id]:
+                    return None,None
+            path.append(actual_node)
+            visited[actual_node] = True
+        return min_capacity,path
+
+    def prepare_result(self):
+        v_min = self.body[self.C:self.C + self.P,-1,f_id]
+        products = self.body[0:self.C,self.C:self.C + self.P,f_id]
+        products[products < 0] = 0
+        v_computed = np.sum(self.body[0:self.C,self.C:self.C + self.P,f_id],axis=0)
+        if np.all(v_computed >= v_min):
+            res = self.body[0:self.C,self.C:self.C + self.P,f_id]
+        else:
+            res = None
+        return res
+
+    def transform(self):
+        self.body[-1,-2,l_id] = 0
+        self.body[-1,-2,u_id] = infinity
+        mask = self.body > 0
+        tmp_body = mask * self.body
+        l_in = np.sum(tmp_body[:,:,l_id],axis=0)
+        l_out = np.sum(tmp_body[:,:,l_id],axis=1)
+        balances = l_in - l_out
+
+        #create new s,t
+        new_body = np.ones((self.body.shape[0]+2,self.body.shape[1]+2,4))*-1
+        new_body[:-2,:-2,:] = self.body
+        self.body = new_body
+
+        #add new edges based on balances
+        for i,b in enumerate(balances):
+            if b > 0:
+                s_id = -2
+                c_id = i
+                self.body[s_id, c_id, l_id] = 0
+                self.body[s_id, c_id, u_id] = b
+            elif b < 0:
+                t_id = -1
+                c_id = i
+                self.body[c_id,t_id, l_id] = 0
+                self.body[c_id,t_id, u_id] = -b
+
+        #change the rest of l
+        self.body[:,:,ld_ori_id] = self.body[:,:,l_id]
+        self.body[:,:,u_id] = self.body[:,:,u_id] - self.body[:,:,l_id]
+        self.body[:,:,l_id][self.body[:,:,l_id] > 0] = 0
+        self.body[:, :, u_id][self.body[:, :, l_id] != 0] = -1
+        self.body[self.body < 0] = - infinity
+        self.num_nodes += 2
+        print("finished transform")
+
+
+def load_data(path2file):
+    with open(path2file,"r") as f:
+        C, P = map(int, f.readline().split())  # n_cust, n_prod
+        graph = Graph(C+P+2, C, P)   # add t and s
+        s_id = C+P
+        t_id = C+P + 1
+        for i in range(C):
+            c_id = i
+            line = [int(x) for x in f.readline().split()]
+            graph.body[s_id, c_id, l_id] = line[0]
+            graph.body[s_id, c_id, u_id] = line[1]
+            for p in line[2:]:
+                p_id = C + p-1
+                graph.body[c_id,p_id,l_id] = 0
+                graph.body[c_id, p_id, u_id] = 1
+        line = [int(x) for x in f.readline().split()]
+
+        for i,v_i in enumerate(line):
+            p_id = i + C
+            graph.body[p_id,t_id, l_id] = v_i
+            graph.body[p_id,t_id, u_id] = infinity
+    return graph
+
+
+def safe_result(path2file,res):
+    with open(path2file, "w") as f:
+        if res is None:
+            f.write(str(-1))
+        else:
+            for r in res:
+                indices = np.where(r == 1)[0] + 1
+                if indices.size != 0:            # Get the indices where the value is equal to 1
+                    f.write(' '.join(map(str, indices)) + '\n')
+    f.close()
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 3:
+        # Default file names
+        path2input_file = "in2.txt"
+        path2output_file = "out.txt"
+    else:
+        path2input_file = sys.argv[1]
+        path2output_file = sys.argv[2]
+
+    g = load_data(path2input_file)                      # OK
+    g_original = load_data(path2input_file)             # OK 1:00
+
+    print(g.body[:, :, 0])
+
+    g.transform()                                       # OK 3:23
+    g.init_flow()                                       # P
+    g.ford_fulkerson()
+    result =g.body[:,:,f_id] + g.body[:,:,ld_ori_id]
+
+    g_original.init_flow(result[:-2,:-2])
+    g_original.ford_fulkerson()
+    r = g_original.prepare_result()
+    safe_result(path2output_file,r)
+
+    print("end")
